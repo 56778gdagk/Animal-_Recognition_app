5bede066fc2765e9d9bbadc15bf51ba8d48f8d2c
56778gdagk <91785115+56778gdagk@users.noreply.github.com>
2022-09-26 21:12:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,5 +11,19 @@ function modelReady()
 
 function gotResults()
 {
-    
+    if(error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+
+        radom_number_r = Math.floor(Math.random() * 255) + 1 ;
+        radom_number_g = Math.floor(Math.random() * 255) + 1 ;
+        radom_number_b = Math.floor(Math.random() * 255) + 1 ;
+
+        document.getElementById("result_label").innerHtml='I can hear - ' + results[0].label;
+        document.getElementById("result_label").innerHtml='I can hear - ' + (results[0].confidence * 100).toFixed(2);
+    }
 }
